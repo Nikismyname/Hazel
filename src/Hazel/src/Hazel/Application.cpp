56f3eca1e0eee245c761cf87e4479737d426dc1d
Nikismyname <nikismyname@abv.bg>
2020-05-30 08:43:55 +0300

    5) Entry Point

---
+++ src/Hazel/src/Hazel/Application.cpp
@@ -0,0 +1,21 @@
+#include "Application.h"
+
+namespace Hazel
+{
+	void Application::Run()
+	{
+		while (true)
+		{
+
+		}
+	}
+
+	Application::Application()
+	{
+	}
+
+	Application::~Application()
+	{
+
+	}
+}
